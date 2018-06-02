0147ff4a723b8d71fee450a6dc3ac2f5b8045ef7
Kevin <kewilliams86@gmail.com>
2018-06-01 23:25:37 -0400

    added '\n' for readability

--- basic_pubmed_parse.py
+++ basic_pubmed_parse.py
@@ -1,5 +1,5 @@
 import pubmed_parser as pp
 pubmed_dict = pp.parse_medline_xml('pubmedsample18n0001.xml')
 for item in pubmed_dict:
-    print(item['title'] + item['abstract'] + item['journal'] + 
-          item['author'] + item['pubdate'] + '\n')
+    print(item['title'] + '\n' + item['abstract'] + '\n' + item['journal'] + '\n' +
+          item['author'] + '\n' + item['pubdate'] + '\n')
