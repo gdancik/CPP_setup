8d51441fb886bb7d6509b6b78770d7e0eff975cc
Garrett Dancik <garrett.dancik@gmail.com>
2018-07-25 08:45:06 -0400

    Added input and output arguments to getPMIDS

--- getPMIDS.py
+++ getPMIDS.py
@@ -8,10 +8,26 @@ Created on Fri Jun 22 08:14:21 2018
 
 import os
 import glob
+import argparse
+import sys
 
+# main program
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser(description='Extract PMIDs from all *.txt files in a directory')
+ap.add_argument("inputDirectory", help = "directory of input files")
+ap.add_argument("outputFile", help = "name of output file")
+
+# print help if no arguments are provided
+if len(sys.argv)==1:
+    ap.print_help(sys.stderr)
+    sys.exit(1)
+
+args = vars(ap.parse_args())
+
+directory = args['inputDirectory']
+outFile = args['outputFile']
 
 # create file listing all PMIDs for text files in directory
-directory = "CPP_articles"
 fileNames = glob.glob(directory+"/*.txt")
 
 allPMIDs = []
@@ -23,9 +39,8 @@ for file in fileNames :
     allPMIDs = allPMIDs + pmids
     f.close()
 
-f = open("pmids.txt", "w")
+f = open(outFile, "w")
 [f.write(x+"\n") for x in allPMIDs]
 f.close()
 
-
-
+print(len(allPMIDs), "pmids written to file: ", outFile)
